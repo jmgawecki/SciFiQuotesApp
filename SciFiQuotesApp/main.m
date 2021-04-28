996189802a3e8d13e8ec45dbacce1b6f020ef4bb
Jakub Gawecki <71930261+jmgawecki@users.noreply.github.com>
2021-04-28 17:46:44 +0100

    Initial Commit

---
+++ SciFiQuotesApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SciFiQuotesApp
+//
+//  Created by Jakub Gawecki on 28/04/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
